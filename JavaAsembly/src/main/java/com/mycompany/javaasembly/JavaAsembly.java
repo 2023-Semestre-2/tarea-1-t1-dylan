7dd39418d773e70b63fdc863da5074b1ead88b59
Dylan Stef Torres Walker <48809822+StefWalker@users.noreply.github.com>
2023-08-28 21:37:11 -0600

    Add files via upload

---
+++ JavaAsembly/src/main/java/com/mycompany/javaasembly/JavaAsembly.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaasembly;
+
+/**
+ *
+ * @author dylan
+ */
+public class JavaAsembly {
+
+    public static void main(String[] args) {
+        UI frame = new UI();
+        frame.show();
+    }
+}
